b7b8f5660a7ce8877f07614b9d111267a0a2c74a
Adriano Prates <adrianopratesr@gmail.com>
2023-08-28 23:44:45 -0300

    feat: ex-1

---
+++ software-engineer-I/fc-1/javascript/modulo-3/ex-1.js
@@ -0,0 +1,9 @@
+const base = 5;
+const height = 2;
+
+const calculateTriangleArea = (base, height) => {
+  const area = (base * height) / 2;
+  return area;
+};
+
+console.log(calculateTriangleArea(base, height));
