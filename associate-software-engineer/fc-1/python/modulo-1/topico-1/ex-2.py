c3e0690e998490ad398d0a79e69df3dd93a50799
Adriano Prates <adrianopratesr@gmail.com>
2023-09-02 02:00:53 -0300

    feat: ex-2

---
+++ associate-software-engineer/fc-1/python/modulo-1/topico-1/ex-2.py
@@ -0,0 +1,6 @@
+x = 5
+# A linha acima, atribui valor a uma variavel isso quer dizer que x vale 5
+y = x + 2
+# A linha acima, declara que a variavel y é o valor que foi atribuido a x mais dois
+print(y)
+# A linha acima, immprime no terminal o valor da variavel y
