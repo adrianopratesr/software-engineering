677a711fa2b1720dac1ccb97690b820d6f240653
Adriano Prates <adrianopratesr@gmail.com>
2023-09-02 02:04:06 -0300

    feat: ex-3

---
+++ associate-software-engineer/fc-1/python/modulo-1/topico-1/ex-3.py
@@ -0,0 +1,8 @@
+x = 3
+# A variavel acima declara que x tem o valor igual a tres
+y = 4
+# A variavel acima declara que y tem o valor igual a 4
+z = 5
+# A variavel acima declara que z tem o valor igual a 5
+a = x + y + z
+# A variavel acima declara que a é o valor da soma da variavel x mais z mais y
