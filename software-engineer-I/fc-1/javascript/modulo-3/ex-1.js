e48ae31bb81cc788da4db592ebf8dc05c56f8563
Adriano Prates <adrianopratesr@gmail.com>
2023-09-02 03:31:34 -0300

    fix: fixed repository folders

--- software-engineer-I/fc-1/javascript/modulo-3/ex-1.js
+++
@@ -1,9 +0,0 @@
-const base = 5;
-const height = 2;
-
-const calculateTriangleArea = (base, height) => {
-  const area = (base * height) / 2;
-  return area;
-};
-
-console.log(calculateTriangleArea(base, height));
--- software-engineer-I/fc-1/javascript/modulo-3/ex-8.js
+++
@@ -1,9 +0,0 @@
-const salary = 4000;
-const increase = 35;
-
-const calculateSalaryIncrease = (salary, increase) => {
-  const percentageIncrease = 1 + increase / 100;
-  return (salary *= percentageIncrease);
-};
-
-console.log(calculateSalaryIncrease(salary, increase));
