39af3c77da95c20a746d5bbaff65c7650f7ec032
Adriano Prates <adrianopratesr@gmail.com>
2023-09-02 02:16:22 -0300

    feat: ex-12

---
+++ associate-software-engineer/fc-1/python/modulo-1/topico-1/ex-12.py
@@ -0,0 +1,7 @@
+base = 10
+# A variavel declara acima atribui o valor da base do retangulo igual a 10
+HEIGHT = 5
+# A constante declara acima atribui o valor da altura do retangulo igual a 5
+
+calculate_perimeter_rectangle = 2 * (base + HEIGHT)
+# A varivel declarada acima calcula o perimetro do retangulo
