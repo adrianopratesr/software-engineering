192b092b714df17807761b67129f0b2c99cd6c34
Adriano Prates <adrianopratesr@gmail.com>
2023-09-28 00:08:45 -0300

    feat: recomendando atividades com base no clima

---
+++ associate-software-engineer/fc-1/javascript/modulo-1/exercise-5.js
@@ -0,0 +1,11 @@
+const climate = "Neve";
+
+if (climate === "Ensolarado") {
+  return console.log("Vá para a praia");
+} else if (climate === "Chuvoso") {
+  return console.log("Leia um livro");
+} else if (climate === "Nublado") {
+  return console.log("Vá ao cinema");
+} else if (climate === "Neve") {
+  return console.log("Faça um boneco de neve");
+}
