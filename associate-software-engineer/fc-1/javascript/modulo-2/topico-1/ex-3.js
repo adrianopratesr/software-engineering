8719a4418763fdb019ba4174bf4a6746e86c0e52
Adriano Prates <adrianopratesr@gmail.com>
2023-10-17 19:06:38 -0300

    feat: ex-4.js

---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-1/ex-3.js
@@ -0,0 +1,6 @@
+const array = ["oi", "tchau", "mau", "cão", "lata"];
+
+array.pop();
+array.shift();
+
+console.log(array);
---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-1/ex-4.js
@@ -0,0 +1,14 @@
+const array = [
+  "oi",
+  "tchau",
+  "mau",
+  "cão",
+  "lata",
+  "Luana",
+  "Adriano",
+  2,
+  3,
+  4,
+];
+
+console.log(array.slice(3, 6));
