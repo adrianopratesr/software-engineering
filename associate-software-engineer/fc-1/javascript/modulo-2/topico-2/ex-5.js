eb6ce00179e2339c65c2d74911b292005048eb83
Adriano Prates <adrianopratesr@gmail.com>
2023-11-14 00:44:10 -0300

    feat: add ex-5

---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-5.js
@@ -0,0 +1,11 @@
+let book = {
+  title: "O programador pragmatico",
+  author: "Andrew Hunt",
+  year: 1999,
+};
+
+console.log(book);
+
+for (let properties in book) {
+  console.log(`${properties} -> ${book[properties]}`);
+}
