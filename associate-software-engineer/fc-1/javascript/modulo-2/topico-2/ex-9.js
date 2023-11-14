04f0c3ef0b75bb79a1eebf621f8c9fb8b64e122e
Adriano Prates <adrianopratesr@gmail.com>
2023-11-14 01:08:03 -0300

    feat: add ex-9

---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-9.js
@@ -0,0 +1,28 @@
+let cars = [
+  {
+    brand: "BMW",
+    model: "M2",
+    year: 2020,
+  },
+  {
+    brand: "mercedes",
+    model: "slr amg",
+    year: 2009,
+  },
+  {
+    brand: "cardilac",
+    model: "escalade",
+    year: 2024,
+  },
+];
+
+console.log(cars);
+
+const newProperty = cars.map((car) => {
+  return {
+    ...car,
+    available: true,
+  };
+});
+
+console.log(newProperty);
