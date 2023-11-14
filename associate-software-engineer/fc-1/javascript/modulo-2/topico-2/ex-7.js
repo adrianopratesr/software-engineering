4f2ba1e4a85f01512e45b7e72947680d58bb153f
Adriano Prates <adrianopratesr@gmail.com>
2023-11-14 00:48:35 -0300

    feat: add ex-7

---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-7.js
@@ -0,0 +1,19 @@
+let car = [
+  {
+    brand: "BMW",
+    model: "M2",
+    year: 2024,
+  },
+  {
+    brand: "mercedes",
+    model: "slr amg",
+    year: 2009,
+  },
+  {
+    brand: "cardilac",
+    model: "escalade",
+    year: 2024,
+  },
+];
+
+console.log(car);
