eaa764bb7838c673f65b97136e852d2fda84be2b
Adriano Prates <adrianopratesr@gmail.com>
2023-12-12 00:17:18 -0300

    fix: fixed comments

--- associate-software-engineer/fc-1/javascript/modulo-3/topico-1/ex-3.js
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-1/ex-3.js
@@ -1,11 +1,11 @@
-function sumSimple(numberA, numberB) {
+function simpleSum(numberA, numberB) {
   return numberA + numberB;
 }
 
 const sumArrow = (numberA, numberB) => numberA + numberB;
 
 console.log(
-  `Com uma função simples ${sumSimple(
+  `Com uma função simples ${simpleSum(
     2,
     3
   )}, realizando com uma arrow function ${sumArrow(2, 3)}`
