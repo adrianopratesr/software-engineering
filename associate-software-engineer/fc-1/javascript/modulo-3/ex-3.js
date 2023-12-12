3a6dbc658d8844a9cb0b7bd270753babc851f91a
Adriano Prates <adrianopratesr@gmail.com>
2023-12-11 23:20:03 -0300

    feat: ex-3

---
+++ associate-software-engineer/fc-1/javascript/modulo-3/ex-3.js
@@ -0,0 +1,12 @@
+const sumSimple = function (numberA, numberB) {
+  return numberA + numberB;
+};
+
+const sumArrow = (numberA, numberB) => numberA + numberB;
+
+console.log(
+  `Com uma função simples ${sumSimple(
+    2,
+    3
+  )}, realizando com uma arrow function ${sumArrow(2, 3)}`
+);
