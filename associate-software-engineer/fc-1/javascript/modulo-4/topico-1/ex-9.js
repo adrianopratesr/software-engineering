cb8a912ccce0235cbf60229ab16ca2f1d378c7d7
Adriano Prates <adrianopratesr@gmail.com>
2023-12-30 15:28:49 -0300

    feat: ex-9

---
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-9.js
@@ -0,0 +1,15 @@
+const calculateArea = (base, height) => {
+    if (!base || !height) {
+      throw new ErrorEntrada("Entrada invalida, favor atribuir valores para base e altura");
+    }
+    if (typeof base !== "number" || typeof height !== "number") {
+        throw new ErrorEntrada ("Entrada invalida, favor inserir apenas numeros entre virgulas para base a altura");
+    }
+    return `A área do retangulo é: ${base * height}`;
+  };
+
+  try {
+    calculateArea(4, "oi")
+  } catch (error) {
+    console.error(error.message);
+  };
