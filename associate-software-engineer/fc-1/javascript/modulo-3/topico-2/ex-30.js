cd456cfdd3898c953ec8f30b49240001a912b8c7
Adriano <admin@MacBook-Air-de-Admin.local>
2023-12-30 01:01:35 -0300

    fix: comments
    fix: comments

--- associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-30.js
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-30.js
@@ -3,10 +3,7 @@ import { students } from "./mock.js";
 const filteringStudentAges = (students, initialAge, endAge) => {
   return students.filter((student) => {
     return student.age >= initialAge && student.age <= endAge
-    
   });
-
-
 };
 
 console.log(filteringStudentAges(students, 30, 65));
