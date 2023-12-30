0b26e5b55fd757c888c39a2601c29f61cba1802e
Adriano Prates <adrianopratesr@gmail.com>
2023-12-30 01:22:47 -0300

    feat: ex-5

---
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-4.js
@@ -0,0 +1,10 @@
+
+
+try{
+    const fragrance = undefined;
+    const fragranceCreator = fragrance.creator; 
+}catch (error){
+    if (error instanceof TypeError) {
+        console.error(error.message);
+    }
+}
---
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-5.js
@@ -0,0 +1,16 @@
+const log = (type, message, details) => {
+    if (type === info){
+        return console.log(type , message, details)
+    } else if(type === error) {
+        return console.error(type, message, details)
+    }else {
+        throw new Error("retorne um erro que seja info ou error");
+    }
+}
+
+
+try{
+    log("info", "oi", {number: 21, numer2: 22})
+}catch(error){
+    console.error(error.message)
+}
