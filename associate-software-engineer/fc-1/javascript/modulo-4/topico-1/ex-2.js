f7212e35b58ebda935de83ad0913e236793c1d79
Adriano Prates <adrianopratesr@gmail.com>
2023-12-31 01:09:08 -0300

    feat: comments 4

--- associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-2.js
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-2.js
@@ -1,6 +1,6 @@
 const sum = (number1, number2) => {
     if (number1 <= 0 || number2 <= 0) {
-        throw new Error("Somente é aceito números positivos");
+        throw new Error("Somente são aceitos números positivos");
     }
     return number1 + number2;
 };
