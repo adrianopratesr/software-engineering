786e4296fb731f50c9fe4831cf2515b0abbcf945
Adriano Prates <adrianopratesr@gmail.com>
2023-12-31 01:32:45 -0300

    feat: comments 10

--- associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-9.js
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-9.js
@@ -9,7 +9,7 @@ const calculateArea = (base, height) => {
   };
 
   try {
-    console,log(`A área do retâtangulo é ${calculateArea(4, "oi")}`);
+    console.log(`A área do retâtangulo é ${calculateArea(4, 6)}`);
   } catch (error) {
     console.error(error.message);
   };
