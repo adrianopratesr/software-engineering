88e6de3b348ea877488e477e135c24b2dec6246d
Adriano Prates <adrianopratesr@gmail.com>
2023-12-30 21:05:13 -0300

    fix: comments 1

--- associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-6.js
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-6.js
@@ -2,11 +2,11 @@ const closeConnection = () => console.log("Fechadno conexão");
 const openConnection = () => console.log("Abrindo conexão");
 
 try {
+    openConnection();
     console.log("Executando operação crítica...");
     throw new Error("Falha na operação!");
 } catch (error) {
     console.error("Erro capturado: " + error.message);
 } finally {
     closeConnection();
-    openConnection();
 }
