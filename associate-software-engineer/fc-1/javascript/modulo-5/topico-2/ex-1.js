c1a3ad1800b72a5299b1eb757faea38408611767
Adriano Prates <adrianopratesr@gmail.com>
2024-01-08 22:11:05 -0300

    feat: ex-1

---
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-2/ex-1.js
@@ -0,0 +1,10 @@
+const helloPromisses = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Olá, promisses!");
+    }, 2000);
+  });
+  
+  helloPromisses.then(message => {
+    console.log(message);
+  });
+  
