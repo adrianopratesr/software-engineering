81f5680208c58a2caf1a226742fa25a26f07c512
Adriano Prates <adrianopratesr@gmail.com>
2024-01-14 14:16:18 -0300

    feat: add ex-3

--- associate-software-engineer/fc-1/javascript/modulo-5/topico-2/ex-2.js
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-2/ex-2.js
@@ -1,15 +1,14 @@
 const helloPromisses = new Promise((resolve, reject) => {
-    const sum = 1 + 3;
+  const sum = 1 + 3;
 
-    setTimeout(() => {
-        if (sum !== 5) {
-          reject("Deu ruim!");
-        };
-        resolve("A soma é 5");
-    }, 1000);
-  });
-  
-  helloPromisses.catch(err => {
-    console.error(err);
-  });
-  
+  setTimeout(() => {
+    if (sum !== 5) {
+      reject("Deu ruim!");
+    }
+    resolve("A soma é 5");
+  }, 1000);
+});
+
+helloPromisses.catch((err) => {
+  console.error(err);
+});
