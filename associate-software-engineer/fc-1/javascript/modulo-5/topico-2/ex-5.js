fea055c0390eeea8bf7f5fe9ef2b34c33d986357
Adriano Prates <adrianopratesr@gmail.com>
2024-01-14 16:08:30 -0300

    feat: add ex-6

--- associate-software-engineer/fc-1/javascript/modulo-5/topico-2/ex-5.js
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-2/ex-5.js
@@ -1,4 +1,3 @@
 fetch("https://randomuser.me/api/")
   .then((result) => result.json())
-  .then((reusltJson) => console.log(reusltJson))
-  .catch((err) => console.error(err));
+  .then((reusltJson) => console.log(reusltJson));
