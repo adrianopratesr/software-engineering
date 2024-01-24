62964550fa29d8e15c1103258e12a80991e105e4
Adriano Prates <adrianopratesr@gmail.com>
2024-01-23 22:27:25 -0300

    feat: add ex-8

--- associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-8.js
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-8.js
@@ -1,23 +1,31 @@
-function randomNumber() {
+const divisaoNumero = () => {
   return new Promise((resolve) => {
-    const randomValue = Math.floor(Math.random() * 10);
-    setTimeout(() => resolve(randomValue), 1000);
+    setTimeout(() => {
+      resolve(10 / 5);
+    }, 2000);
   });
-}
+};
 
-function sumNumber() {
+const somarNumeros = () => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(2 + 5), 2000);
+    setTimeout(() => {
+      resolve(2 + 5);
+    }, 2000);
   });
-}
+};
 
-function mutiplySum() {
+const multiplicarSoma = () => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(2 * 2), 3000);
+    setTimeout(() => {
+      resolve(2 * 2);
+    }, 3000);
   });
-}
+};
 
-Promise.all([randomNumber(), sumNumber(), mutiplySum()]).then((results) => {
-  const [result1, result2, result3] = results;
-  console.log(result1, result2, result3);
-});
+const principal = async () => {
+  const [resultadoDivisao, resultadoSoma, resultadoMultiplicacao] =
+    await Promise.all([divisaoNumero(), somarNumeros(), multiplicarSoma()]);
+  console.log(resultadoDivisao, resultadoSoma, resultadoMultiplicacao);
+};
+
+principal();
